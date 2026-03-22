cdf5f42dd00d3c31e9291afdb5ae9713daf46e0c
deebo8070 <natinael.mulu1@gmail.com>
2026-03-21 18:07:18 -0600

    made a name change and added a rout to inreviewSetupPage

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,17 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Assets
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';
+import heroImg from './assets/hero.png';
+import './App.css';
+
+// Pages
+import InterviewSetupPage from './pages/InterviewSetupPage';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <p>This is an app.</p>
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Your default Vite template acts as the home page for now */}
+        <Route path="/" element={
+          <div className="App">
+            <p>This is an app.</p>
+          </div>
+        } />
+
+        {/* Your new setup route! */}
+        <Route path="/setup" element={<InterviewSetupPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
